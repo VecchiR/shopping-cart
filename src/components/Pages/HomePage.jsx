94ca9931394dfefc8537cd09a9e6097c530e6d52
Vecchi <79600745+VecchiR@users.noreply.github.com>
2024-11-25 08:42:37 -0300

    Update homepage component structure

--- src/components/Pages/HomePage.jsx
+++ src/components/Pages/HomePage.jsx
@@ -1,10 +1,18 @@
 import { Link } from 'react-router-dom';
+import Header from '../Header';
+import GoShopButton from '../GoShopButton';
 
 export default function HomePage() {
   return (
-    <>
-      <p>THIS IS THE HOMEPAGE</p>
-      <Link to="/store">Go shop!</Link>
-    </>
+    <div>
+      <img src="" alt="someImage" />
+      <div className="text-content">
+        <h1>Name of the store</h1>
+        <p>Little description, slogan, whatever</p>
+      </div>
+      <Link to="/store">
+        <GoShopButton />
+      </Link>
+    </div>
   );
 }
