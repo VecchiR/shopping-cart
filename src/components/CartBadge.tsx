b2a9722e7b7688890ffdac5c1979439ad1866887
Vecchi <79600745+VecchiR@users.noreply.github.com>
2024-11-26 14:19:15 -0300

    Show total quantity of items on cart at the header

--- src/components/CartBadge.tsx
+++ src/components/CartBadge.tsx
@@ -2,13 +2,15 @@ import React, { useContext } from 'react';
 import { ShopContext } from './App';
 
 export default function CartBadge() {
-
   const { cart } = useContext(ShopContext); // We must pass the ShopContext object itself as an argument
 
+  const totalQuantity = cart.reduce((total, item) => total + item.quantity, 0);
 
   return (
     <>
-      <div onClick={() => console.log(cart)}>CartBadge <span>{cart.length}</span></div>
+      <div onClick={() => console.log(cart)}>
+        CartBadge <span>{totalQuantity}</span>
+      </div>
     </>
   );
 }
