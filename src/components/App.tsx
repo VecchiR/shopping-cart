6d26929aa77bb19e876716f90ad1f7c5506af310
Vecchi <79600745+VecchiR@users.noreply.github.com>
2024-11-26 14:04:01 -0300

    Type-check App and implement addProductToCart function

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,16 +7,17 @@ import Header from './Header';
 import ProductCard from './ProductCard';
 import Footer from './Footer';
 import ProductPage from './Pages/ProductPage';
+import { CartItem, Product, ShopContextType } from '../../types';
 
-export const ShopContext = createContext({
+export const ShopContext = createContext<ShopContextType>({
   products: [],
   cart: [],
   addProductToCart: () => {},
 });
 
 export default function App() {
-  const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [cart, setCart] = useState<CartItem[]>([]);
 
   useEffect(() => {
     fetch('https://fakestoreapi.com/products', { mode: 'cors' })
@@ -25,16 +26,33 @@ export default function App() {
       .catch((error) => console.error(error));
   }, []);
 
-  const addProductToCart = (productId) => {
-    console.log('productId:', productId);
-    const product = products.find((product) => product.id === parseInt(productId));
-    setCart([...cart, product]);
+  const addProductToCart = (productId: number, quantity: number = 1): void => {
+    //check if there's already a product with that id in the cart
+    const cartItem = cart.find((item) => item.product.id === productId);
+
+    if (cartItem) {
+      // Update the quantity of the existing cart item
+      setCart((prevCart) =>
+        prevCart.map((item) =>
+          item.product.id === productId ? { ...item, quantity: item.quantity + quantity } : item
+        )
+      );
+    } else {
+      // Find the product in the products array
+      const product = products.find((product) => product.id === productId);
+      if (product) {
+        // Add the product to the cart with the specified quantity
+        setCart((prevCart) => [...prevCart, { product, quantity }]);
+      } else {
+        console.error(`Product with ID ${productId} not found`);
+      }
+    }
   };
 
   return (
     <>
       <ShopContext.Provider value={{ cart, products, addProductToCart }}>
-        <Header cart={cart} />
+        <Header/>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route
