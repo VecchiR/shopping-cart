f338cb964ac2a304d1c6669873c229b6f4a0344b
Vecchi <79600745+VecchiR@users.noreply.github.com>
2024-12-20 15:05:35 -0300

    Adapt header for mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,29 @@ import { NavLink } from 'react-router-dom';
 import CartBadge from './CartBadge';
 
 export default function Header() {
+  // return (
+  //   <header className="p-6 sticky top-0 z-50 bg-white">
+  //     <nav className="grid grid-cols-custom-layout justify-items-center justify-between items-center">
+  //       <div className="flex justify-self-end gap-12">
+  //         <NavLink to="/" className={({ isActive }) => (isActive ? 'underline' : '')}>
+  //           Home
+  //         </NavLink>
+  //         <NavLink to="/store" className={({ isActive }) => (isActive ? 'underline' : '')}>
+  //           Store
+  //         </NavLink>
+  //       </div>
+  //       <NavLink to="/">
+  //         <h1 className="font-lime font-thin text-4xl">dress.CODE</h1>
+  //       </NavLink>
+  //       <CartBadge />
+  //     </nav>
+  //   </header>
+  // );
+
   return (
-    <header className="p-6 sticky top-0 z-50 bg-white">
-      <nav className="grid grid-cols-custom-layout justify-items-center justify-between items-center">
-        <div className="flex justify-self-end gap-12">
+    <header className='bg-white p-3'>
+      <nav className='flex justify-evenly items-center'>
+        <div className='flex flex-row gap-4'>
           <NavLink to="/" className={({ isActive }) => (isActive ? 'underline' : '')}>
             Home
           </NavLink>
@@ -15,10 +34,11 @@ export default function Header() {
           </NavLink>
         </div>
         <NavLink to="/">
-          <h1 className="font-lime font-thin text-4xl">dress.CODE</h1>
+          <h1 className='font-lime font-thin text-[1.7rem]'>dress.CODE</h1>
         </NavLink>
         <CartBadge />
       </nav>
     </header>
   );
+
 }
