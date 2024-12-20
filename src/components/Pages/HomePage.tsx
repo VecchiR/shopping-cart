c47adce3d2205bd871c5e4f067363c1ca6e97768
Vecchi <79600745+VecchiR@users.noreply.github.com>
2024-12-20 15:05:31 -0300

    Upd filters on homepage bg img

--- src/components/Pages/HomePage.tsx
+++ src/components/Pages/HomePage.tsx
@@ -4,7 +4,7 @@ import GoShopButton from '../GoShopButton';
 export default function HomePage() {
   return (
     <div className='relative flex flex-col flex-1 justify-center items-center h-full'>
-      <div className='absolute inset-0 bg-cover bg-[url("/src/assets/bg.jpg")] brightness-75  -z-1'></div>
+      <div className='absolute inset-0 bg-cover bg-[url("/src/assets/bg.jpg")] brightness-75 contrast-[0.65]  -z-1'></div>
       <div className='relative flex flex-col items-center z-10'>
         <h1 className='font-hill font-thin  text-8xl text-white'>Style is the code</h1>
       </div>
