ac24c9c18d14a182f8c93f8f661d3f2ba8fe5bc3
Vecchi <79600745+VecchiR@users.noreply.github.com>
2024-12-20 15:20:57 -0300

    Adapt Homepage to mobile

--- src/components/Pages/HomePage.tsx
+++ src/components/Pages/HomePage.tsx
@@ -2,15 +2,29 @@ import { Link } from 'react-router-dom';
 import GoShopButton from '../GoShopButton';
 
 export default function HomePage() {
+  // return (
+  //   <div className='relative flex flex-col flex-1 justify-center items-center h-full'>
+  //     <div className='absolute inset-0 bg-cover bg-[url("/src/assets/bg.jpg")] brightness-75 contrast-[0.65]  -z-1'></div>
+  //     <div className='relative flex flex-col items-center z-10'>
+  //       <h1 className='font-hill font-thin  text-8xl text-white'>Style is the code</h1>
+  //     </div>
+  //     <Link to="/store" className='relative z-10'>
+  //       <GoShopButton />
+  //     </Link>
+  //   </div>
+  // );
+
   return (
     <div className='relative flex flex-col flex-1 justify-center items-center h-full'>
-      <div className='absolute inset-0 bg-cover bg-[url("/src/assets/bg.jpg")] brightness-75 contrast-[0.65]  -z-1'></div>
-      <div className='relative flex flex-col items-center z-10'>
-        <h1 className='font-hill font-thin  text-8xl text-white'>Style is the code</h1>
+      <div className='absolute inset-0 bg-cover bg-center bg-[url("/src/assets/bg.jpg")] brightness-75 contrast-[0.65]  -z-1'></div>
+      <div className='relative flex flex-col items-center'>
+        <h1 className='font-hill font-thin text-6xl text-white'>Style is the code</h1>
       </div>
-      <Link to="/store" className='relative z-10'>
+      <Link to="/store" className='relative'>
         <GoShopButton />
       </Link>
     </div>
   );
+
+
 }
